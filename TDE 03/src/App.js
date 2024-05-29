0a52f65ecc8f99f8771406c37f8dee1518fe97dd
Pedro Antônio Dos Santos <139076592+PedroA-Santos@users.noreply.github.com>
2024-05-29 20:58:09 -0300

    Add files via upload

---
+++ TDE 03/src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+
+      <div>
+      <h1 className="Title1"><b>Kyan & Mu540: os quebradas inteligentes</b></h1>
+      </div>
+
+      <div>
+      <p className="p1"><b>Crias da Baixada Santista, MC e produtor consolidam a parceria no EP “UM Quebrada Inteligente”, uma viagem dançante que mescla house, drill e funk</b>
+      </p>
+      </div>
+
+      <div>
+      <img  className="imagem1" src='https://monkeybuzz.com.br/wp-content/uploads/2023/08/capa-mu540.jpg' alt='Kyan'/>
+      </div>
+
+
+      <div>
+        <p className="p1">Um dos fundamentos tanto do rap quanto do funk é a combinação MC e DJ. A união entre o que está de frente para o público desferindo rimas e a figura que fica na retaguarda soltando beats e impulsionando melodias pode resultar em hits e parcerias de sucesso – de Thaíde & DJ Hum a MC Duda do Marapé & DJ Marquinhos Sangue Bom. No caso de Kyan & Mu540, responsáveis pelo EP UM Quebrada Inteligente, a colaboração vai além da música e é uma parceria pessoal. Uma amizade que se torna som e que traz ecos vindos da Baixada Santista, local onde ambos os artistas viveram a efervescência do funk e descobriram os caminhos que desejavam trilhar.</p>
+      </div>
+
+      
+
+
+
+    </div>
+
+
+  );
+}
+
+export default App;
